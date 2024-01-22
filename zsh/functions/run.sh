a60575c1dde25d54af88b9bdf2e8c16274e269a9
gustavolimav <gustavo.lima@liferay.com>
2024-01-22 10:54:31 -0300

    Unseting NPM_CONFIG_PREFIX

--- zsh/functions/run.sh
+++ zsh/functions/run.sh
@@ -5,3 +5,5 @@ zsh_add_file "$ZSH_DIR/functions/work/java.sh"
 zsh_add_file "$ZSH_DIR/functions/work/others.sh"
 zsh_add_file "$ZSH_DIR/functions/work/properties.sh"
 zsh_add_file "$ZSH_DIR/functions/work/solr.sh"
+
+unset NPM_CONFIG_PREFIX # Adding this so I can use NVM
