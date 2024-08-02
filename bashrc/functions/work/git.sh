17b9e1fdab4e2eb84ac94c89558b60c59980c0c7
gustavolimav <gustavo.lima@liferay.com>
2024-08-02 13:32:53 -0300

    fix

--- bashrc/functions/work/git.sh
+++ bashrc/functions/work/git.sh
@@ -1,61 +1,53 @@
 function parse_git_branch {
-			GIT_DIR_NAME=$(git rev-parse --show-toplevel)
+	GIT_DIR_NAME=$(git rev-parse --show-toplevel)
 
-			GIT_DIR_NAME=${GIT_DIR_NAME##*/}
+	GIT_DIR_NAME=${GIT_DIR_NAME##*/}
 
-			if [[ "${GIT_DIR_NAME}" =~ -ee-[0-9][^\-]*$ ]]
-			then
-				echo "ee-${GIT_DIR_NAME##*-ee-}"
-			elif [[ "${GIT_DIR_NAME}" =~ -[0-9][^\-]*$ ]]
-			then
-				echo "${GIT_DIR_NAME##*-}"
-			elif [[ "${GIT_DIR_NAME}" =~ com-liferay-osb-asah-private$ ]]
-			then
-				echo "7.0.x"
-			elif [[ "${GIT_DIR_NAME}" =~ -private$ ]]
-			then
-				echo "${GIT_DIR_NAME}" | sed 's/.*-\([^\-]*\)-private$/\1-private/'
-			else
-				echo "master"
-			fi
+	if [[ "${GIT_DIR_NAME}" =~ -ee-[0-9][^\-]*$ ]]; then
+		echo "ee-${GIT_DIR_NAME##*-ee-}"
+	elif [[ "${GIT_DIR_NAME}" =~ -[0-9][^\-]*$ ]]; then
+		echo "${GIT_DIR_NAME##*-}"
+	elif [[ "${GIT_DIR_NAME}" =~ com-liferay-osb-asah-private$ ]]; then
+		echo "7.0.x"
+	elif [[ "${GIT_DIR_NAME}" =~ -private$ ]]; then
+		echo "${GIT_DIR_NAME}" | sed 's/.*-\([^\-]*\)-private$/\1-private/'
+	else
+		echo "master"
+	fi
 }
 
 function git_home() {
-    local git_root
-    git_root=$(git rev-parse --show-toplevel 2> /dev/null)
-    if [[ -z "$git_root" ]]; then
-        echo "Not a git repository."
-    else
-        cd "$git_root" || return
-    fi
+	local git_root
+	git_root=$(git rev-parse --show-toplevel 2>/dev/null)
+	if [[ -z "$git_root" ]]; then
+		echo "Not a git repository."
+	else
+		cd "$git_root" || return
+	fi
 }
 
 function git_grep {
-	if [ ${#} -eq 1 ]
-	then
+	if [ ${#} -eq 1 ]; then
 		git --no-pager grep --files-with-matches "${1}"
-	elif [ ${#} -eq 2 ]
-	then
+	elif [ ${#} -eq 2 ]; then
 		git --no-pager grep --files-with-matches "${1}" -- "${2}"
-	elif [ ${#} -eq 3 ]
-	then
+	elif [ ${#} -eq 3 ]; then
 		git --no-pager grep --files-with-matches "${1}" -- "${2}" "${3}"
-	elif [ ${#} -eq 4 ]
-	then
+	elif [ ${#} -eq 4 ]; then
 		git --no-pager grep --files-with-matches "${1}" -- "${2}" "${3}" "${4}"
 	fi
 }
 
 function git_init() {
 	if [ -z "$1" ]; then
-			printf "%s\n" "Please provide a directory name.";
+		printf "%s\n" "Please provide a directory name."
 	else
-			mkdir "$1";
-			builtin cd "$1";
-			pwd;
-			git init;
-			touch readme.md .gitignore LICENSE;
-			echo "# $(basename $PWD)" >> readme.md
+		mkdir "$1"
+		builtin cd "$1"
+		pwd
+		git init
+		touch readme.md .gitignore LICENSE
+		echo "# $(basename $PWD)" >>readme.md
 	fi
 }
 
@@ -125,23 +117,19 @@ function commit_with_pattern() {
 	git commit -m "${ticketCode} ${folderName}: ${commitMessage}" -m "${jiraLink}"
 }
 
-
 function git_fetch_pr {
 	if [[ "${1}" != */github\.com/* ]] ||
-	   [[ "${1}" != */pull/* ]]
-	then
+		[[ "${1}" != */pull/* ]]; then
 		echo "URL ${1} does not point to a GitHub pull request."
 	else
-		IFS='/' read -r -a github_pr_parts <<< "${1}"
+		IFS='/' read -r -a github_pr_parts <<<"${1}"
 
 		git fetch --no-tags git@github.com:${github_pr_parts[3]}/${github_pr_parts[4]}.git pull/${github_pr_parts[6]}/head:pr-${github_pr_parts[6]}
 	fi
 }
 
-#!/bin/bash
-
 function check_git_available() {
-	if ! command -v git &> /dev/null; then
+	if ! command -v git &>/dev/null; then
 		echo "git command not found. Please install git."
 		return 1
 	fi
@@ -149,7 +137,7 @@ function check_git_available() {
 }
 
 function check_inside_git_repo() {
-	if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+	if ! git rev-parse --is-inside-work-tree &>/dev/null; then
 		echo "Not inside a Git repository. Please navigate to a Git repository."
 		return 1
 	fi
@@ -157,16 +145,14 @@ function check_inside_git_repo() {
 }
 
 function get_ticket_code_from_commit() {
-	local ticketCode=$1
-	if [[ -z "$ticketCode" ]]; then
-		ticketCode=$(git log -1 --pretty=format:%s | awk '{print $1}')
-	fi
+	ticketCode=$(git log -1 --pretty=format:%s | awk '{print $1}')
+
 	echo "$ticketCode"
 }
 
 function create_rebase_script() {
 	local rebaseScript=$(mktemp)
-	cat << EOF > "$rebaseScript"
+	cat <<EOF >"$rebaseScript"
 #!/bin/bash
 sed -i 's/^pick /edit /' "\$1"
 EOF
@@ -175,12 +161,39 @@ EOF
 }
 
 function amend_commit_message() {
-	local ticketCode=$1
-	local jiraLink=$2
+	check_git_available || return 1
+	check_inside_git_repo || return 1
 
+	local ticketCode=$1
+
+	if [[ -z "$ticketCode" ]]; then
+		ticketCode=$(get_ticket_code_from_commit "$ticketCode")
+	fi
+
+	if [[ ! $ticketCode =~ ^LPD- ]]; then
+		echo "Invalid ticket code. Ticket code must start with LPD-."
+		return
+	fi
+
+	local jiraLink="https://liferay.atlassian.net/browse/${ticketCode}"
+
+	local folderName=$(find_foulder_with_lfrbuild)
+
+	local commitMessage=$(git log --format=%B -n 1 HEAD)
+	local pattern="${ticketCode} ${folderName}:"
+
+	if [[ $commitMessage == "$pattern"* ]]; then
+		echo "Commit message already follows the pattern."
+		return
+	fi
+
+	git commit --amend --no-edit --message="${ticketCode} ${folderName}: $commitMessage" --message="${jiraLink}"
+}
+
+function find_foulder_with_lfrbuild() {
 	local commitHash=$(git log --format=%H -n 1 HEAD)
 	local folderName=$(git diff-tree --no-commit-id --name-only -r "$commitHash" | head -n 1 | xargs dirname)
-	local maxIterations=10
+	local maxIterations=20
 	local iterationCount=0
 
 	while [[ ! -f "$folderName/.lfrbuild-portal" && "$folderName" != "/" && $iterationCount -lt $maxIterations ]]; do
@@ -194,30 +207,20 @@ function amend_commit_message() {
 
 	folderName=$(basename "$folderName")
 
-	local commitMessage=$(git log --format=%B -n 1 HEAD)
-	local pattern="${ticketCode} ${folderName}:"
-	if [[ $commitMessage == "$pattern"* ]]; then
-		echo "Commit message already follows the pattern."
-		return
-	fi
-
-	git commit --amend --no-edit --message="${ticketCode} ${folderName}: $commitMessage" --message="${jiraLink}"
+	echo "$folderName"
 }
 
 function apply_pattern_to_commits() {
 	check_git_available || return 1
 	check_inside_git_repo || return 1
 
-	local ticketCode=$1
-	local numCommits=$2
+	local numCommits=$1
+	local ticketCode=$2
 
 	if [[ -z "$numCommits" ]]; then
 		echo "Number of commits must be specified."
 		return 1
 	fi
-
-	ticketCode=$(get_ticket_code_from_commit "$ticketCode")
-	local jiraLink="https://liferay.atlassian.net/browse/${ticketCode}"
 
 	local rebaseScript=$(create_rebase_script)
 
@@ -229,7 +232,7 @@ function apply_pattern_to_commits() {
 	fi
 
 	for i in $(seq 1 $numCommits); do
-		amend_commit_message "$ticketCode" "$jiraLink"
+		amend_commit_message "$ticketCode"
 		git rebase --continue
 
 		if [[ $? -ne 0 ]]; then
