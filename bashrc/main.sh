d33334a788da171b766822f06dd3f82343810da4
gustavolimav <gustavo.lima@liferay.com>
2024-08-20 15:46:02 -0300

    Add comment

--- bashrc/main.sh
+++ bashrc/main.sh
@@ -15,14 +15,14 @@ source "$BASH_DIR/functions/work/db.sh" # Load functions related to databases
 source "$BASH_DIR/functions/work/bundles.sh" # Load functions related to Liferay workspace setup
 source "$BASH_DIR/functions/work/git.sh" # Load functions related to git
 source "$BASH_DIR/functions/work/java.sh" # Load functions related to Java
-source "$BASH_DIR/functions/work/others.sh" 
+source "$BASH_DIR/functions/work/others.sh" # Load extra functions
 
 customize_prompt
 
 source "$BASH_DIR/functions/work/solr.sh" # Load functions related to Solr
 source "$BASH_DIR/functions/work/opensearch.sh" # Load functions related to OpenSearch
 
-source ~/.local/share/blesh/ble.sh
+source ~/.local/share/blesh/ble.sh # Load Ble.sh further informations here: https://github.com/akinomyoga/ble.sh
 shopt -s autocd
 
 unset NPM_CONFIG_PREFIX # Adding this so I can use NVM
